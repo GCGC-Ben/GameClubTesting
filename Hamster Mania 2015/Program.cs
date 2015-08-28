5eadd6ae08bc4c9f90a120159027d44151e6650c
SfsBen <Starfoxsonic@gmail.com>
2015-08-28 19:23:31 -0400

    committed everything

---
+++ Hamster Mania 2015/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SdlDotNet;
+using SdlDotNet.Core;
+using SdlDotNet.Graphics;
+
+namespace Hamster_Mania_2015
+{
+    class Program
+    {
+          public static void Main(string[] args)
+        {
+            Video.SetVideoMode(320, 240, 32, false, false, false, true);
+            Events.Run();
+        }
+    }
+}
