23cfe4bab5d248292fd59af9942e315308ca07cf
Jackson Whitley <jackson.whitley@bobcats.gcsu.edu>
2016-04-25 19:05:03 -0400

    Added format for animation manager, some for a menu when just wandering the world.

---
+++ MOTB/MOTB/MOTB/AnimationManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace MOTB
+{
+    class AnimationManager
+    {
+
+        List<Texture2D> txtList = new List<Texture2D>();
+
+        int frameNum;
+        int interval;
+
+        public AnimationManager(List<Texture2D> tList)
+        {
+            txtList = tList;
+        }
+
+
+    }
+}
